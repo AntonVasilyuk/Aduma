ba4397437119d79fff49b710441f6aa2ecfdb646
AntonVasilyuk <wrajina99@gmail.com>
2017-02-13 06:01:30 +0500

    JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
 
-public class Calculate{
+/**
+ * Calculate.
+ *
+ * @author Anton Vasilyuk (wrajina99@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Calculate {
+	/**
+     * Main.
+     * @param args - args.
+     */
 	public static void main(String[] args) {
-	System.out.println("Hello World");	
+		System.out.println("Hello World");
 	}
 }
