8ddec95d96984e800b2c652ae937450cef9bc17c
AntonVasilyuk <wrajina99@gmail.com>
2017-02-19 11:45:52 +0500

    add Test for Calculator.java

---
+++ chapter_001/src/test/java/ru/job4j/CalculatorTest.java
@@ -0,0 +1,73 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**.
+ * CalculatorTest.
+ *
+ * @author Anton Vasilyuk (wrajina99@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+ public class CalculatorTest {
+
+	/**.
+	* Тест сложения
+	* @param first
+	* @param second
+	*/
+	@Test
+	public void testWhenAddOneToOneThenTwo() {
+		Calculator calc = new Calculator();
+		calc.add(1, 1);
+		final double result = calc.getResult();
+		assertThat(result, is(2d));
+
+	}
+
+	/**.
+	* Тест Вычитание
+	* @param first
+	* @param second
+	*/
+	@Test
+	public void testWhenSubstructOneToOneThenZero() {
+		Calculator calc = new Calculator();
+		calc.substruct(2, 1);
+		final double result = calc.getResult();
+		assertThat(result, is(1d));
+
+	}
+
+	/**.
+	* Тест Деление
+	* @param first
+	* @param second
+	*/
+	@Test
+	public void testWhenDifOneToOneThenOne() {
+		Calculator calc = new Calculator();
+		calc.dif(1, 1);
+		final double result = calc.getResult();
+		assertThat(result, is(1d));
+
+	}
+
+	/**.
+	* Тест Умножение
+	* @param first
+	* @param second
+	*/
+	@Test
+	public void testWhenMultipleOneToOneThenOne() {
+		Calculator calc = new Calculator();
+		calc.multiple(1, 1);
+		final double result = calc.getResult();
+		assertThat(result, is(1d));
+
+	}
+
+}
