628dcf433330b0941ae2ca64cf37379377bb7c2e
AntonVasilyuk <wrajina99@gmail.com>
2017-02-26 14:58:41 +0500

    add file chapter_001_3.2

---
+++ chapter_001/src/test/java/ru/job4j/PointTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+/**.
+ * PointTest
+ *
+ * @author Anton Vasilyuk
+ * @version 1.0
+ * @since 0.1
+ */
+
+public class PointTest {
+
+	/**.
+	* Тест нахождения расстояния
+	*/
+	@Test
+	public void whenDistanceATOBThenOne() {
+	Point a = new Point(1, 2);
+	Point b = new Point(2, 2);
+
+	double ab = a.distanceTo(a, b);
+
+	assertThat(ab, closeTo(1d, 0.01));
+	}
+}
