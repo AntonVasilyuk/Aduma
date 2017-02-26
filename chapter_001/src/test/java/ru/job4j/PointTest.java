4444258fb34f9ad59decbbec6985cf96860fe553
AntonVasilyuk <wrajina99@gmail.com>
2017-02-26 21:31:49 +0500

    add file for chapter_001_3.2

--- chapter_001/src/test/java/ru/job4j/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/PointTest.java
@@ -22,7 +22,7 @@ public class PointTest {
 	Point a = new Point(1, 2);
 	Point b = new Point(2, 2);
 
-	double ab = a.distanceTo(a, b);
+	double ab = a.distanceTo(b);
 
 	assertThat(ab, closeTo(1d, 0.01));
 	}
