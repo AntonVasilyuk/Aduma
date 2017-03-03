21f5991316e5b8a662fa7e837aa19c7fd8a111ec
AntonVasilyuk <wrajina99@gmail.com>
2017-03-03 22:57:44 +0500

    add file for chapter_001_3.3

--- chapter_001/src/main/java/ru/job4j/Max.java
+++ chapter_001/src/main/java/ru/job4j/Max.java
@@ -13,13 +13,23 @@ public class Max {
 	* сравнение
 	* @param first первый аргумент
 	* @param second второй аргумент
+	* @return возвращает наибольшее значение
+	*/
+	public int max(int first, int second) {
+
+		return first > second ? first : second;
+
+	}
+	/**.
+	* сравнение
+	* @param first первый аргумент
+	* @param second второй аргумент
 	* @param third третий аргумент
 	* @return возвращает наибольшее значение
 	*/
 	public int max(int first, int second, int third) {
 
-		int result = first > second ? first : second;
-		return result > third ? result : third;
+		return max(max(first, second), third);
 
 	}
 
