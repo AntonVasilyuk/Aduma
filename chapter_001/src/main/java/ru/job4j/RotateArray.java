430b1390235988cf55ecb85636a9c26e9ae635ea
AntonVasilyuk <wrajina99@gmail.com>
2017-03-26 18:00:49 +0500

    add file for chapter_001_5.2

--- chapter_001/src/main/java/ru/job4j/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/RotateArray.java
@@ -16,19 +16,18 @@ public class RotateArray {
 	*/
 	public int[][] rotate(int[][] array) {
 		int l = array.length;
-		int[][] antiArray = new int[l][l];
+		int var;
 		for (int x = 0; x < l / 2; x++) {
 			for (int i = x; i < (l - x - 1); i++) {
 				int j = l - 1;
-				antiArray[i][x] = array[j - x][i];
-				antiArray[j - x][i] = array[j - i][j - x];
-				antiArray[j - i][j - x] = array[x][j - i];
-				antiArray[x][j - i] = array[i][x];
+				var = array[i][x];
+				array[i][x] = array[j - x][i];
+				array[j - x][i] = array[j - i][j - x];
+				array[j - i][j - x] = array[x][j - i];
+				array[x][j - i] = var;
 			}
 		}
-		if ((l % 2) != 0) {
-			antiArray[l / 2][l / 2] = array[l / 2][l / 2];
-			}
-		return antiArray;
+
+		return array;
 	}
 }
