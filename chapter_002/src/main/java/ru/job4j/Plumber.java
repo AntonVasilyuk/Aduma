f17dff0678e2189b19379e561dd2eb05d1d5ccb5
AntonVasilyuk <wrajina99@gmail.com>
2017-04-18 13:06:27 +0500

    add file for chapter_002_2.3

---
+++ chapter_002/src/main/java/ru/job4j/Plumber.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+/**.
+*
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
+public class Plumber extends Profession {
+
+	/**.
+	* Constructor for class Plumber
+	* @param n name
+	* @param h it's be education
+	* @param e experiens
+	* @param pay paymant working
+	*/
+	Plumber(String n, boolean h, double e, int pay) {
+		super(n, h, e, pay);
+	}
+
+	/**.
+	* Method work Plumber
+	* @param client for name
+	* @return description
+	*/
+	public String repairPlump(Client client) {
+		String val = "Сантехник " + getName() + " ремонтирует клиенту " + client.getName() + " сантехнику дома за " + getPayment() + " долларов.";
+		return val;
+	}
+}
