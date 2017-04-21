1a17cf19c5a09b7c03acb619a30a2c0ae7cab5b3
AntonVasilyuk <wrajina99@gmail.com>
2017-04-21 09:01:26 +0500

    add file for chapter_002_2.4

--- chapter_002/src/main/java/ru/job4j/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/Tracker.java
@@ -1,38 +1,116 @@
 package ru.job4j;
 
+import java.util.Random;
+
+/**.
+*
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
 public class Tracker {
 	/**.
-	* Array items
+	* @items Array items
 	*/
-	private Item[] items;
-	private position = 0;
+	private Item[] items = new Item[10];
+
+	/**.
+	* @position for array
+	*/
+	private int position = 0;
+
+	/**.
+	* @RN git id
+	*/
+	private static final Random RN = new Random();
 
 	/**.
 	* method for add items
-	* @param Item
+	* @param item item object
+	* @return result
 	*/
 	public Item add(Item item) {
-		this.item[position++] = item;
+		item.setId(String.valueOf(RN.nextInt()));
+		this.items[position++] = item;
 		return item;
 	}
 
+	/**.
+	* method for update items
+	* @param item item for update
+	*/
 	public void update(Item item) {
-		
+		for (int index = 0; index != position; index++) {
+			if (item != null && item.getId().equals(items[index].getId())) {
+				items[index] = item;
+				break;
+			}
+		}
 	}
 
+	/**.
+	* method for delete items
+	* @param item item for delete
+	*/
 	public void delete(Item item) {
-		
+		Item[] result = new Item[this.position];
+		for (int index = 0, i = 0; index != this.position; index++, i++) {
+			if (items[index].equals(item)) {
+				index++;
+			}
+			result[i] = items[index];
+			this.items = result;
+		}
 	}
 
+	/**.
+	* method for find all items
+	* @return result
+	*/
 	public Item[] findAll() {
-		
+		Item[] result = new Item[this.position];
+		for (int index = 0; index != this.position; index++) {
+			result[index] = this.items[index];
+		}
+		return result;
 	}
 
+	/**.
+	* method for find items
+	* @param key name
+	* @return array with dublicate name
+	*/
 	public Item[] findByName(String key) {
-		
+		int i = 0;
+		int num = 0;
+		for (int index = 0; index != position; index++) {
+			if (items[index] != null && items[index].getName().equals(key)) {
+				num++;
+			}
+		}
+		Item[] result = new Item[num];
+		for (int index = 0; index != position; index++) {
+			if (items[index] != null && items[index].getName().equals(key)) {
+				result[i++] = items[index];
+			}
+		}
+		return result;
 	}
 
+	/**.
+	* method for show all items
+	* @param id id for item
+	* @return item
+	*/
 	public Item findById(String id) {
-		
+		Item result = null;
+		for (Item item : items) {
+			if (item != null && item.getId().equals(id)) {
+				result = item;
+				break;
+			}
+		}
+	return result;
 	}
 }
