4ab213763b23984aec54b89903f4fbe330b551bf
AntonVasilyuk <wrajina99@gmail.com>
2017-04-27 19:27:20 +0500

    add file for chapter_002_2.5.4

---
+++ chapter_002/shape/src/main/java/ru/job4j/Paint.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+/**.
+* Chapter_002
+* Task 5.4
+*
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
+public class Paint {
+
+	/**.
+	* Method for test printing image shape
+	* @param shape our shape
+	*/
+	public void draw(Shape shape) {
+		System.out.println(shape.pic());
+	}
+}
