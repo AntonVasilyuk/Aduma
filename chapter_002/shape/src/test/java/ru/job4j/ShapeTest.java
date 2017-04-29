d0aef812590ac75d548c6afda3c6ed4ad8a168cc
AntonVasilyuk <wrajina99@gmail.com>
2017-04-29 08:39:34 +0500

    add file for chapter_002_2.5.4

--- chapter_002/shape/src/test/java/ru/job4j/ShapeTest.java
+++ chapter_002/shape/src/test/java/ru/job4j/ShapeTest.java
@@ -3,6 +3,11 @@ package ru.job4j;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 
 /**.
 * Chapter_002
@@ -15,6 +20,17 @@ import static org.junit.Assert.assertThat;
 
 public class ShapeTest {
 
+	/**.
+	* method for write print
+	* @param fileName name file
+	* @return string from file
+	*/
+	private static String readUsingScanner(String fileName) throws IOException {
+        Scanner scanner = new Scanner(Paths.get(fileName), StandardCharsets.UTF_8.name());
+        String data = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        return data;
+	}
 	/**.
 	* Test method pic for square
 	*/
@@ -37,5 +53,37 @@ public class ShapeTest {
 		String fact = triangle.pic();
 		String expect = "w \nw w \nw w w \n";
 		assertThat(fact, is(expect));
-	} 
+	}
+
+	/**.
+	* Test method draw for square
+	*/
+	@Test
+	public void whenNeedPrintingSquareThenWePrintSquare() throws Exception {
+		int height = 3;
+		Paint paint = new Paint();
+		Square square = new Square(height);
+		FileOutputStream f = new FileOutputStream("file.txt") ;
+		System.setOut(new PrintStream(f));
+		paint.draw(square);
+		String fact = readUsingScanner("file.txt");
+		String expect = "w w w \nw w w \nw w w \n\r\n";
+		assertThat(fact, is(expect));
+	}
+
+	/**.
+	* Test method draw for Triangle
+	*/
+	@Test
+	public void whenNeedPrintingTriangleThenWePrintTriangle() throws Exception {
+		int height = 3;
+		Paint paint = new Paint();
+		Triangle triangle = new Triangle(height);
+		FileOutputStream f = new FileOutputStream("file.txt") ;
+		System.setOut(new PrintStream(f));
+		paint.draw(triangle);
+		String expect = "w \nw w \nw w w \n\r\n";
+		String fact = readUsingScanner("file.txt");
+		assertThat(fact, is(expect));
+	}
 }
