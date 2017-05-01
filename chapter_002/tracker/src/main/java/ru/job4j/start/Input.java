b30c1a89b72393e4551764c9534f164ae42e8ed3
AntonVasilyuk <wrajina99@gmail.com>
2017-05-01 16:38:42 +0500

    modifed file for chapter_002_2.7.1

--- chapter_002/tracker/src/main/java/ru/job4j/start/Input.java
+++ chapter_002/tracker/src/main/java/ru/job4j/start/Input.java
@@ -1,6 +1,16 @@
 package ru.job4j.start;
 
+/**.
+* Chapter_002
+* It's interface for give users enter
+* 
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
 public interface Input {
+	
 	String ask(String question);
 	int ask(String question, int[] ranges);
 }
