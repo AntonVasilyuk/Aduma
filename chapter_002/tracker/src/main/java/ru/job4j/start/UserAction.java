ded727d7669eb5164d57e09f2acd27b64958d188
AntonVasilyuk <wrajina99@gmail.com>
2017-05-01 16:42:18 +0500

    add file for chapter_002_2.6.1

--- chapter_002/tracker/src/main/java/ru/job4j/start/UserAction.java
+++ chapter_002/tracker/src/main/java/ru/job4j/start/UserAction.java
@@ -1,7 +1,17 @@
 package ru.job4j.start;
 
+/**.
+* Chapter_002
+* Task 2.7.1
+* This interface need for creating action for user's
+* 
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
 public interface UserAction {
-	int key();
-	void execute(Input input, Tracker tracker);
-	String info();
+	public int key();
+	public void execute(Input input, Tracker tracker);
+	public String info(String nameAction);
 }
