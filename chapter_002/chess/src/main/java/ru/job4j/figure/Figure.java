8eb0090ef9044c35d2c42f35c332992b11699e19
AntonVasilyuk <wrajina99@gmail.com>
2017-05-12 21:34:13 +0500

    add file for chapter_002_2.9.2

---
+++ chapter_002/chess/src/main/java/ru/job4j/figure/Figure.java
@@ -0,0 +1,38 @@
+package ru.job4j.figure;
+
+import ru.job4j.game.*;
+
+/**.
+* Chapter_002
+* Task 2.9.2
+* Interface for create figure
+*
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
+public abstract class Figure {
+
+
+    /**.
+     * @cellPosition position the sell
+     */
+    private final Cell cellPosition;
+
+    /**.
+     * Constructor
+     * @param position is cell for new figure
+     */
+    public Figure(Cell position){
+        this.cellPosition = position;
+    }
+
+    /**.
+     * Abstract method building way for the figure
+     * @param dist finish point
+     * @return way array cells
+     */
+    abstract Cell[] way(Cell dist) throws ImposibleMoveException;
+
+}
