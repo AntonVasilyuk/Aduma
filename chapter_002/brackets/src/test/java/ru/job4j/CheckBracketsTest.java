5840bdd53a1145e5543757763668b42adc4dae78
AntonVasilyuk <wrajina99@gmail.com>
2017-05-12 21:31:19 +0500

    add file for chapter_002_2.9.1

---
+++ chapter_002/brackets/src/test/java/ru/job4j/CheckBracketsTest.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**.
+* Chapter_002
+* It's class for testing checkBrackets
+* 
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
+public class CheckBracketsTest {
+
+	/**.
+	* Test for method checkBrackets
+	*/
+	@Test
+	public void whenIsArrayBracketsThenValidateThisBrackets() {
+		CheckBrackets brack = new CheckBrackets();
+		char[] brackets = {'(', ')', '(', ')'};
+		boolean expected = true;
+		boolean fact = brack.checkBreckets(brackets);
+		assertThat(fact, is(expected));
+	}
+}
