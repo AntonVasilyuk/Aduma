802dd9893d0d7e37965f096989412d7fac88a558
AntonVasilyuk <wrajina99@gmail.com>
2017-05-14 18:59:55 +0500

    add file for chapter_002_2.9.2

--- chapter_002/chess/src/test/java/ru/job4j/ChessBoardTest.java
+++ chapter_002/chess/src/test/java/ru/job4j/ChessBoardTest.java
@@ -23,17 +23,50 @@ public class ChessBoardTest {
 	/**.
 	* Method for test move figures
 	*/
-	@Test//(expected = ImposibleMoveException.class)
+	@Test
 	public void whenFigureWantMoveThenCheckMayBeMakeThis() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
 		Cell nowCell = new Cell(0, 0);
 		Cell nextCell = new Cell(0, 6);
 		Board board = new Board();
 		board.fillFigure();
-		//Figure[] figures = board.getFigures();
 		boolean result = board.move(nowCell, nextCell);
-		//Bishop bishop = new Bishop(nowCell);
-		//bishop.way(nextCell);
 		boolean expect = true;
 		assertThat(result, is(expect));
+	}
+
+	/**.
+	* Method for test ImposibleMoveException
+	*/
+	@Test(expected = ImposibleMoveException.class)
+	public void whenFigureCantMoveThenGetUpError() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+		Cell nowCell = new Cell(0, 0);
+		Cell nextCell = new Cell(5, 5);
+		Board board = new Board();
+		board.fillFigure();
+		board.move(nowCell, nextCell);
+	}
+
+	/**.
+	* Method for test OccupedWayException
+	*/
+	@Test(expected = OccupedWayException.class)
+	public void whenFigureDistCellOccupedThenGetUpError() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+		Cell nowCell = new Cell(0, 0);
+		Cell nextCell = new Cell(0, 7);
+		Board board = new Board();
+		board.fillFigure();
+		board.move(nowCell, nextCell);
+	}
+
+	/**.
+	* Method for test OccupedWayException
+	*/
+	@Test(expected = FigureNotFoundException.class)
+	public void whenInSourceCellNotFigureThenGetUpError() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+		Cell nowCell = new Cell(0, 6);
+		Cell nextCell = new Cell(0, 5);
+		Board board = new Board();
+		board.fillFigure();
+		board.move(nowCell, nextCell);
 	}
 }
