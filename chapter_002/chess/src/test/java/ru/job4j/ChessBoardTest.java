840f49b673b1800b031020323173fa8acad0f200
AntonVasilyuk <wrajina99@gmail.com>
2017-05-16 22:06:36 +0500

    add file for chapter_002_2.9.2

--- chapter_002/chess/src/test/java/ru/job4j/ChessBoardTest.java
+++ chapter_002/chess/src/test/java/ru/job4j/ChessBoardTest.java
@@ -21,12 +21,12 @@ import static org.junit.Assert.assertThat;
 public class ChessBoardTest {
 
 	/**.
-	* Method for test move figures
+	* Method for test move Castl
 	*/
 	@Test
-	public void whenFigureWantMoveThenCheckMayBeMakeThis() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+	public void whenCastlWantMoveThenCheckMayBeMakeThis() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
 		Cell nowCell = new Cell(0, 0);
-		Cell nextCell = new Cell(0, 6);
+		Cell nextCell = new Cell(6, 0);
 		Board board = new Board();
 		board.fillFigure();
 		boolean result = board.move(nowCell, nextCell);
@@ -63,10 +63,66 @@ public class ChessBoardTest {
 	*/
 	@Test(expected = FigureNotFoundException.class)
 	public void whenInSourceCellNotFigureThenGetUpError() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
-		Cell nowCell = new Cell(0, 6);
+		Cell nowCell = new Cell(3, 6);
 		Cell nextCell = new Cell(0, 5);
 		Board board = new Board();
 		board.fillFigure();
 		board.move(nowCell, nextCell);
+	}
+
+	/**.
+	* Method for test move Bishop
+	*/
+	@Test
+	public void whenBishopWantMoveThenCheckMayBeMakeThis() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+		Cell nowCell = new Cell(0, 2);
+		Cell nextCell = new Cell(2, 4);
+		Board board = new Board();
+		board.fillFigure();
+		boolean result = board.move(nowCell, nextCell);
+		boolean expect = true;
+		assertThat(result, is(expect));
+	}
+
+	/**.
+	* Method for test move Knight
+	*/
+	@Test
+	public void whenKnightWantMoveThenCheckMayBeMakeThis() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+		Cell nowCell = new Cell(0, 1);
+		Cell nextCell = new Cell(2, 2);
+		Board board = new Board();
+		board.fillFigure();
+		boolean result = board.move(nowCell, nextCell);
+		boolean expect = true;
+		assertThat(result, is(expect));
+	}
+
+	/**.
+	* Method for test move Queen
+	*/
+	@Test
+	public void whenQueenWantMoveThenCheckMayBeMakeThis() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+		Cell nowCell = new Cell(0, 3);
+		Cell nextCell = new Cell(5, 3);
+		Board board = new Board();
+		board.fillFigure();
+		boolean result = board.move(nowCell, nextCell);
+		boolean expect = true;
+		assertThat(result, is(expect));
+	}
+
+	/**.
+	* Method for test move King
+	*/
+	@Test
+	public void whenKingWantMoveThenCheckMayBeMakeThis() throws ImposibleMoveException, OccupedWayException, FigureNotFoundException, CloneNotSupportedException {
+		Cell nowCell = new Cell(0, 4);
+		Cell nextCell = new Cell(1, 3);
+		Board board = new Board();
+		board.fillFigure();
+		boolean result = board.move(nowCell, nextCell);
+		boolean expect = true;
+		assertThat(result, is(expect));
 	}
 }
