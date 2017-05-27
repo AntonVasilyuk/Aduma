5c91060bee788b3f07887af2a69fa065471d0a79
AntonVasilyuk <wrajina99@gmail.com>
2017-05-27 11:11:20 +0500

    add file for chapter_003_3.1.3

---
+++ chapter_003/checkTime/src/main/java/ru/job4j/CheckTime.java
@@ -0,0 +1,51 @@
+package ru.job4j;
+
+import java.util.*;
+
+/**.
+* Chapter_003
+* Task 3.1.2
+* Check time for other collection
+*
+* @author Anton Vasilyuk
+* @version 1.0
+* @since 0.1
+*/
+
+public class CheckTime {
+
+	/**.
+	* Method for check time for operation add
+	* @param collection
+	* @param line is added word
+	* @param amount is number element
+	* @return time
+	*/
+	public long add(Collection<String> collection, String line, int amount) {
+
+		String word = "a";
+		long firstTime = System.currentTimeMillis();
+
+		for (int i = 0; i < amount; i++) {
+			word += line;
+			collection.add(word);
+		}
+		long secondTime = System.currentTimeMillis();
+		return secondTime - firstTime;
+	}
+	public long delete(Collection<String> collection, int amount) {
+
+		Iterator<String> iter = collection.iterator();
+		int i = 0;
+		long firstTime = System.currentTimeMillis();
+
+		while (iter.hasNext() && i < amount) {
+			iter.next();
+			iter.remove();
+			i++;
+		}
+
+		long secondTime = System.currentTimeMillis();
+		return secondTime - firstTime;
+	}
+}
