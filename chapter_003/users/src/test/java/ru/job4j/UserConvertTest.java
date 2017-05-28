796ba4a4f01f3779361212f46e0cf146206bd552
AntonVasilyuk <wrajina99@gmail.com>
2017-05-28 08:24:10 +0500

    add file for chapter_003_3.2.2

--- chapter_003/users/src/test/java/ru/job4j/UserConvertTest.java
+++ chapter_003/users/src/test/java/ru/job4j/UserConvertTest.java
@@ -1,7 +1,41 @@
 package ru.job4j;
 
-/**
- * Created by ANTON on 28.05.2017.
+import java.util.*;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**.
+ * Chapter_003
+ * Task 3.2.2
+ * Test create collection HashMap
+ *
+ * @author Anton Vasilyuk
+ * @version 1.0
+ * @since 0.1
  */
+
 public class UserConvertTest {
+    /**.
+     * Method for test method convert from class UserConvert
+     */
+    @Test
+    public void whenIsListUsersThenConvertIsToMap() {
+        UserConvert userconv = new UserConvert();
+        HashMap<Integer, User> hashMapUsers = new HashMap<>();
+        List<User> list = new LinkedList<>();
+        User userOne = new User(1, "Boris", "Moscow");
+        User userTwo = new User(2, "Ivan", "Novosibirsk");
+        User userThird = new User(3, "Gregory", "Dacota");
+        list.add(userOne);
+        list.add(userTwo);
+        list.add(userThird);
+        hashMapUsers = userconv.process(list);
+        boolean fact = false;
+        boolean expect = true;
+        if (hashMapUsers.containsKey(1) && hashMapUsers.containsKey(2) && hashMapUsers.containsKey(3)) {
+            fact = true;
+        }
+        assertThat(fact, is(expect));
+    }
 }
