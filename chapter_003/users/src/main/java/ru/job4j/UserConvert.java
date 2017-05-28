f2e6edae0e1fcff4b51892f02ffa1429d32f1cce
AntonVasilyuk <wrajina99@gmail.com>
2017-05-28 08:23:47 +0500

    add file for chapter_003_3.2.2

--- chapter_003/users/src/main/java/ru/job4j/UserConvert.java
+++ chapter_003/users/src/main/java/ru/job4j/UserConvert.java
@@ -1,7 +1,29 @@
 package ru.job4j;
 
-/**
- * Created by ANTON on 28.05.2017.
+import java.util.*;
+
+/**.
+ * Chapter_003
+ * Task 3.2.2
+ * Create collection HashMap
+ *
+ * @author Anton Vasilyuk
+ * @version 1.0
+ * @since 0.1
  */
 public class UserConvert {
+    /**.
+     * Method for convert from List to HashMap
+     * @param list
+     * @return
+     */
+    public HashMap<Integer, User> process(List<User> list) {
+        Integer id;
+        HashMap<Integer, User> hashMapUsers = new HashMap<>();
+        for (User user: list) {
+            id = user.getId();
+            hashMapUsers.put(id, user);
+        }
+        return hashMapUsers;
+    }
 }
