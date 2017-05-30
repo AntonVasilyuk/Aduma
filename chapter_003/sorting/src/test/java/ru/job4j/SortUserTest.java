5e866b2e250cb04679da0398db3ee2af871f2d1a
AntonVasilyuk <wrajina99@gmail.com>
2017-05-30 07:29:52 +0500

    modification file for chapter_003_3.3.2

--- chapter_003/sorting/src/test/java/ru/job4j/SortUserTest.java
+++ chapter_003/sorting/src/test/java/ru/job4j/SortUserTest.java
@@ -1,7 +1,90 @@
 package ru.job4j;
 
+import java.util.*;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * Created by ANTON on 30.05.2017.
+ * Chapter_003
+ * Task 3.3.1
+ * Test sorting array users by name
+ *
+ * @author Anton Vasilyuk
+ * @version 1.0
+ * @since 0.1
  */
 public class SortUserTest {
+
+    /**.
+     * Test sorting by age
+     */
+    @Test
+    public void whenAddArrayThenGetSortingArray() {
+        SortUser sortUser = new SortUser();
+        List<User> list = new ArrayList<>();
+        Set<User> sortingList = new TreeSet<>();
+        User one = new User("Ivan", "23");
+        User two = new User("Boris", "20");
+        User three = new User("Vladimir", "55");
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        sortingList = sortUser.sort(list);
+        String fact = "Result sorting : ";
+        for (User user : sortingList) {
+            fact += user.getAge();
+        }
+        String expect = "Result sorting : 202355";
+        assertThat(fact, is(expect));
+
+    }
+
+    /**.
+     * Test sorting by hashCod
+     */
+    @Test
+    public void whenAddArrayThenGetSortingArrayByHashCod() {
+        SortUser sortUser = new SortUser();
+        List<User> list = new ArrayList<>();
+        List<User> sortingList = new ArrayList<>();
+        User one = new User("Ivan", "23");
+        User two = new User("Boris", "20");
+        User three = new User("Vladimir", "55");
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        sortingList = sortUser.sortHash(list);
+        String fact = "Result sorting : ";
+        for (User user : sortingList) {
+            fact += user.getAge();
+        }
+        String expect = "Result sorting : 205523";
+        assertThat(fact, is(expect));
+
+    }
+
+    /**.
+     * Test sorting by hashCod
+     */
+    @Test
+    public void whenAddArrayThenGetSortingArrayByLengthName() {
+        SortUser sortUser = new SortUser();
+        List<User> list = new ArrayList<>();
+        List<User> sortingList = new ArrayList<>();
+        User one = new User("Ivan", "23");
+        User two = new User("Boris", "20");
+        User three = new User("Vladimir", "55");
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        sortingList = sortUser.sortLength(list);
+        String fact = "Result sorting : ";
+        for (User user : sortingList) {
+            fact += user.getAge();
+        }
+        String expect = "Result sorting : 232055";
+        assertThat(fact, is(expect));
+
+    }
 }
