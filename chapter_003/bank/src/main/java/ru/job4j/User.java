f5a3b81f364121388cf243540170a1502400465a
AntonVasilyuk <wrajina99@gmail.com>
2017-06-02 07:45:38 +0500

    add file for chapter_003_3.4.2

--- chapter_003/bank/src/main/java/ru/job4j/User.java
+++ chapter_003/bank/src/main/java/ru/job4j/User.java
@@ -1,7 +1,69 @@
 package ru.job4j;
 
 /**
- * Created by ANTON on 31.05.2017.
+ * Testing Task 3.4.2
+ * Create collection Map for bank
+ *
+ * @author Anton Vasilyuk
+ * @version 1.0
+ * @since 0.1
  */
 public class User {
+    /**.
+     * name user
+     */
+    private String name;
+
+    /**.
+     * passport user
+     */
+    private Integer passport;
+
+    /**.
+     * Constructor for create user
+     * @param name
+     * @param passport
+     */
+    public User(String name, Integer passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+    /**.
+     * equals users by name
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return this.name.equals(user.name);
+    }
+
+    /**.
+     * hashCod users
+     * @return hashCod users
+     */
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    /**.
+     * Getter name
+     * @return name user
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**.
+     * Getter passport user
+     * @return passport user
+     */
+    public Integer getPassport() {
+        return this.passport;
+    }
 }
