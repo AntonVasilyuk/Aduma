d8f4e9664dbd239780ad85c2f69935cc8533534c
AntonVasilyuk <wrajina99@gmail.com>
2017-06-03 08:30:52 +0500

    add file for chapter_003_4.1.2

--- chapter_003/iteratorEvenNumbers/src/main/java/ru/job4j/IteratorEvenNumbers.java
+++ chapter_003/iteratorEvenNumbers/src/main/java/ru/job4j/IteratorEvenNumbers.java
@@ -10,10 +10,48 @@ import java.util.Iterator;
  * @version 1.0
  * @since 0.1
  */
-public class iterEvenNumbers implements Iterator {
 
+public class IteratorEvenNumbers implements Iterator {
+
+    /**.
+     * @values is array numbers
+     */
+    private int[] values;
+
+    /**.
+     * @index is index array
+     */
+    private int index = 0;
+
+    /**.
+     * Constructor for class IteratorEvenNumber
+     * @param values is array numbers
+     */
+    public IteratorEvenNumbers(int[] values) {
+        this.values = values;
+    }
+
+    /**.
+     * Check latest index
+     * @return boolean
+     */
     @Override
     public boolean hasNext() {
-        return false;
+        return this.values.length > index;
+    }
+
+    /**
+     * Realisation iterator for even number
+     * @return object on this position
+     */
+    @Override
+    public Object next() {
+        if(hasNext()) {
+            while ((this.values[index] % 2) != 0) {
+                index++;
+                }
+            }
+        return this.values[index++];
     }
 }
+
