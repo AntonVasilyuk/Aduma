dd509236fb9c0a3d5702d271cb11ec63402684ca
AntonVasilyuk <wrajina99@gmail.com>
2017-06-03 08:33:31 +0500

    add file for chapter_003_4.1.1

---
+++ chapter_003/iteratorArray/src/main/java/ru/job4j/IteratorArray.java
@@ -0,0 +1,61 @@
+package ru.job4j;
+
+import java.util.*;
+/**
+ * Task 4.1.1
+ * Create Iterator array
+ *
+ * @author Anton Vasilyuk on 03.06.2017.
+ * @version 1.0
+ * @since 0.1
+ */
+
+public class IteratorArray implements Iterator {
+
+    /**.
+     * @value array numbers
+     */
+    private int[][] value;
+
+    /**.
+     * @index this array
+     */
+    private int index = 0;
+
+    /**.
+     * @colon this array
+     */
+    private int colon = 0;
+
+    /**.
+     * Constructor class Iterator array
+     * @param value
+     */
+    public IteratorArray(int[][] value) {
+        this.value = value;
+    }
+
+    /**.
+     * Check latest index
+     * @return boolean
+     */
+    @Override
+    public boolean hasNext() {
+        return this.value.length > index;
+    }
+
+    /**.
+     * Realisation iterator for multidimensional array
+     * @return number array
+     */
+    @Override
+    public Object next() {
+        if(hasNext()) {
+            return this.value[colon][index++];
+        } else {
+            index = 0;
+            colon++;
+            return this.value[colon][index++];
+        }
+    }
+}
