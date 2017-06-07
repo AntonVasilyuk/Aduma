673862e50a8c644522739c607c28823da339c84f
AntonVasilyuk <wrajina99@gmail.com>
2017-06-07 07:53:27 +0500

    add file for chapter_003_5.2.1

--- chapter_003/genericOne/src/main/java/ru/job4j/SimpleList.java
+++ chapter_003/genericOne/src/main/java/ru/job4j/SimpleList.java
@@ -1,5 +1,7 @@
 package ru.job4j;
 
+import java.util.NoSuchElementException;
+
 /**
  * Task 5.2.1
  * Create generic
@@ -8,7 +10,35 @@ package ru.job4j;
  * @version 1.0
  * @since 0.1
  */
-public class GenericOne {
+public class SimpleList<E> {
 
-    private 
+    private Object[] objects;
+    private int index = 0;
+
+    public SimpleList(int size) {
+        this.objects = new Object[size];
+    }
+
+    public <K> K print(K value) {
+        System.out.println(value);
+        return value;
+    }
+
+    public void add(E value) {
+        this.objects[index++] = value;
+    }
+
+    public E get (int position) {
+        return (E) this.objects[position];
+    }
+
+    public void update(int position, E value) {
+        if (value != null) {
+            this.objects[position] = value;
+        } else {throw new NullPointerException("this value is null");}
+    }
+
+    public void delete(int position) {
+        this.objects[position] = null;
+    }
 }
