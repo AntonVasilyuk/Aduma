c7db64f041971e55fbd81bb80b78bedc1d15078f
AntonVasilyuk <wrajina99@gmail.com>
2017-06-10 07:57:59 +0500

    add file for chapter_003_4.1.3

--- chapter_003/iteratorSimpleNumbers/src/main/java/ru/job4j/IteratorSimpleNumbers.java
+++ chapter_003/iteratorSimpleNumbers/src/main/java/ru/job4j/IteratorSimpleNumbers.java
@@ -9,60 +9,113 @@ import java.util.*;
  * @version 1.0
  * @since 0.1
  */
-public class IteratorSimpleNumbers implements Iterator{
+public class IteratorSimpleNumbers implements Iterator {
 
-    /**.
+    /**
+     * .
+     *
      * @index is index array
      */
     private int index = 0;
 
-    /**.
+    /**
+     * .
+     * Colon array
+     */
+    int colon = 0;
+
+    /**
+     * .
+     *
      * @value is array numbers
      */
-    private int[] value;
+    private int[][] values;
 
-    /**.
+    /**
+     * .
      * Constructor for class Iterator SimpleNumbers
+     *
      * @param value is array numbers
      */
-    public IteratorSimpleNumbers(int[] value) {
-        this.value = value;
+    public IteratorSimpleNumbers(int[][] value) {
+        this.values = value;
     }
 
-    /**.
+    /**
+     * .
      * Method for check next member array
+     *
      * @return is there a next element
      */
     @Override
     public boolean hasNext() {
-        boolean result = true;
-        if (this.value == null) {throw new NoSuchElementException("In this array not element");}
-        if (this.value.length < index) {result = false;}
-        int num = 0;
-        for (int i = 0; i < value.length; i++) {
-            for(int j = 2; j < value.length; j++) {
-                if((value[i] % j) == 0) {num++;}
+
+        if (this.values == null) {
+            throw new NoSuchElementException("In this array not element");
+        }
+        int checkNum = 0;
+        for (int i = 0; i < this.values.length; i++) {
+            for (int j = 0; j < this.values[i].length; j++) {
+                int checkSimple = 1;
+                for (int x = 2; x < this.values[i][j]; x++) {
+                    if ((this.values[i][j] % x) == 0) {
+                        checkSimple = 0;
+                    }
+                }
+                if (checkSimple == 1) {
+                    checkNum++;
+                }
             }
         }
-        if (num > 0) {result = false;}
-        return result;
+        if (checkNum == 0) {
+            return false;
+        }
+        if (!(this.values.length > colon)) {
+            return false;
+        }
+        if (this.values[colon].length > index) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /**.
+    /**
+     * .
      * Method for output next simple number
+     *
      * @return simple number
      */
     @Override
     public Object next() {
-        int num = 0;
-        while (hasNext()) {
-            for (int i = 2; i < this.value[index]; i++) {
-                if((this.value[index] % i) == 0) {num++;}
+        int num;
+        if (hasNext()) {
+            do {
+                num = 1;
+                for (int i = 2; i < this.values[colon][index]; i++) {
+                    if ((this.values[colon][index] % i) == 0) {
+                        num = 0;
+                    }
+                }
+                if (num > 0) {break;}
+                index++;
+            } while (num > 0);
+        } else {
+            colon++;
+            index = 0;
+            if (hasNext()) {
+                do {
+                    num = 1;
+                    for (int i = 2; i < this.values[colon][index]; i++) {
+                        if ((this.values[colon][index] % i) == 0) {
+                            num = 0;
+                        }
+                    }
+                    if (num > 0) {break;}
+                    index++;
+                } while (num > 0);
             }
-            if(num == 0){break;}
-            index++;
-            num = 0;
         }
-    return this.value[index++];
+        return this.values[colon][index++];
     }
 }
