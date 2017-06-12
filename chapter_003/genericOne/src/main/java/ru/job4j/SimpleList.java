c2fbf546b8233736549367f82596c0fcb0c14d8e
AntonVasilyuk <wrajina99@gmail.com>
2017-06-12 07:49:06 +0500

    add file for chapter_003_5.2.1

--- chapter_003/genericOne/src/main/java/ru/job4j/SimpleList.java
+++ chapter_003/genericOne/src/main/java/ru/job4j/SimpleList.java
@@ -15,10 +15,15 @@ public class SimpleList<T> {
 
     private Object[] objects;
     private int index = 0;
+    private static final int defaultSize = 10;
 
     public SimpleList(int size) {
         this.objects = new Object[size];
      }
+
+    public SimpleList() {
+        this.objects = new Object[defaultSize];
+    }
 
     public void add(T value) {
         if (value == null) {throw new NullPointerException("Element is null");}
