40ee000356a5d68a3fb45f7944f516cb5a3e1f0c
AntonVasilyuk <wrajina99@gmail.com>
2017-06-16 15:16:50 +0500

    add file for chapter_003_5.3.4

---
+++ chapter_003/list/src/main/java/ru/job4j/CycleList.java
@@ -0,0 +1,134 @@
+package ru.job4j;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * Task 5.3.4.
+ * Create check for cyclicity
+ *
+ * Created by ANTON VASILYUK on 16.06.2017.
+ * @version 1.0
+ */
+
+public class CycleList<T> {
+
+    private Node<T> hadNode;
+    private Node<T> previos;
+    private Node<T> nowNode;
+    private int size = 0;
+
+    public void add(T value) {
+        if (value == null) {throw new NullPointerException("Element is null");}
+        if (hadNode == null) {
+            Node<T> node = new Node(value, null, null);
+            hadNode = node;
+            previos = node;
+            nowNode = node;
+            this.size++;
+        } else {
+            Node<T> node = new Node(value, previos, null);
+            previos.setNext(node);
+            nowNode = node;
+            this.size++;
+        }
+    }
+
+    public boolean hasCycle(){
+        boolean result = false;
+        List<T> list = new LinkedList<>();
+        Node<T> temp = hadNode;
+        int checkOfNum = 1;
+        if (temp != null) {
+            while(checkOfNum == 1) {
+                checkOfNum = 0;
+                list.add(temp.value);
+                for(T t : list) {
+                    if (t == temp.value) {checkOfNum++;}
+                }
+
+                if (checkOfNum > 1) {result = true;}
+
+                if (temp.getNext() != null) {temp = temp.getNext();}
+                else {break;}
+            }
+        } else {throw new NoSuchElementException("It list is empty");}
+        if (size == 1) {throw new NoSuchElementException("Add more element!");}
+        return result;
+    }
+
+    public void makeCycle() {
+        nowNode.setNext(hadNode);
+        hadNode.setPrevios(nowNode);
+    }
+
+    class Node<T> {
+
+        /**.
+         * @value is element for storage
+         */
+        private T value;
+
+        /**.
+         * Link on previos node
+         */
+        private Node<T> first;
+
+        /**.
+         * Link on next node
+         */
+        private Node<T> last;
+
+        /**.
+         * Constructor for Node
+         * @param value is element for storage
+         * @param previos link on previos node
+         * @param next link on next node
+         */
+        public Node(T value, Node<T> previos, Node<T> next) {
+            this.value = value;
+            this.first = previos;
+            this.last = next;
+        }
+
+        /**.
+         * Method set previos node
+         * @param previos is previos node
+         */
+        public void setPrevios(Node<T> previos) {
+            this.first = previos;
+        }
+
+        /**.
+         * Method set next node
+         * @param next is next node
+         */
+        public void setNext(Node<T> next) {
+            this.last = next;
+        }
+
+        /**.
+         * Method for get previos node
+         * @return link previos node
+         */
+        public Node<T> getPrevios() {
+            return this.first;
+        }
+
+        /**.
+         * Method for get next node
+         * @return link next node
+         */
+        public Node<T> getNext() {
+            return this.last;
+        }
+
+        /**.
+         * Method for get storage element
+         * @return this element
+         */
+        public T getElement() {return this.value;}
+    }
+}
+
---
+++ chapter_003/list/src/test/java/ru/job4j/CycleListTest.java
@@ -0,0 +1,68 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Task 5.3.4.
+ * Test check for cyclicity
+ *
+ * Created by ANTON VASILYUK on 16.06.2017.
+ * @version 1.0
+ */
+public class CycleListTest {
+
+    /**.
+     * Test check on cyclicity
+     */
+    @Test
+    public void whenCheckOnCyclicityList() {
+
+        CycleList list = new CycleList();
+        Integer one = 1;
+        Integer two = 2;
+        Integer tree = 3;
+        Integer four = 4;
+        list.add(one);
+        list.add(two);
+        list.add(tree);
+        list.add(four);
+
+        list.makeCycle();
+        boolean result = list.hasCycle();
+
+        assertThat(result, is(true));
+    }
+
+    /**.
+     * Test if no element
+     */
+    @Test
+    public void whenListIsEmpty() {
+        CycleList list = new CycleList();
+        try {
+            list.hasCycle();
+        } catch (NoSuchElementException nee) {
+            assertThat(nee.getMessage(), is("It list is empty"));
+        }
+    }
+
+    /**.
+     * Test if only one element
+     */
+    @Test
+    public void whenOnlyOneElement() {
+        CycleList list = new CycleList();
+        Integer one = 99;
+        list.add(one);
+        try {
+            list.hasCycle();
+        } catch (NoSuchElementException nee) {
+            assertThat(nee.getMessage(), is("Add more element!"));
+        }
+    }
+}
