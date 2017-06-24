6a1fd4afdbf9bad02ea552ad817350a30da36d6d
AntonVasilyuk <wrajina99@gmail.com>
2017-06-24 08:41:51 +0500

    add file for chapter_003_5.5.8

--- chapter_003/map/src/main/java/ru/job4j/MyMap.java
+++ chapter_003/map/src/main/java/ru/job4j/MyMap.java
@@ -1,14 +1,17 @@
 package ru.job4j;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
- * Task 5.5.2.
+ * Task 5.5.2. and Task 5.5.8.
  * Create my map
  *
  * Created by ANTON VASILYUK on 21.06.2017.
  * @version 1.0
  */
 
-public class MyMap<U, E> {
+public class MyMap<U, E> implements Iterable<Entry>{
 
     /**.
      * @size is size array for the map
@@ -18,7 +21,12 @@ public class MyMap<U, E> {
     /**.
      * @cursor is position cursor
      */
-    public int cursor;
+    public int cursor = 0;
+
+    /**.
+     * @cursor for iterator
+     */
+    public int cursorIter = 0;
 
     /**.
      * @map is container for items
@@ -40,37 +48,44 @@ public class MyMap<U, E> {
      * @param value is element for adding
      * @return old element with this key
      */
-    public E put(U key, E value) {
-        E tempValue = null;
+    public boolean insert(U key, E value) {
+        boolean result = false;
+
         if (key == null) {throw new NullPointerException("Key is null");}
         if (value == null) {throw new NullPointerException("Element is null");}
         if (cursor == 0) {
             map[0] = new Entry(key, value);
             cursor++;
+            return true;
         } else {
-            if(cursor < size) {
-                for (int i = 0; i < cursor; i++) {
-                    if (map[i].getUser().hashCode() == key.hashCode() && map[i].getUser().equals(key)) {
-                        tempValue = (E) map[i];
-                        map[i].setObject(value);
-                    } else {map[cursor++] = new Entry(key, value);}
+            int position = searchInsert(key, map, 0, cursor - 1);
+            if (position != -1) {
+                if(this.cursor < this.size / 100 * 75) {
+                    for (int i = this.cursor; i > position; i--) {
+                        this.map[i] = this.map[i - 1];
+                    }
+                    this.map[position] = new Entry(key, value);
+                    this.cursor++;
+                    result = true;
+                } else {
+                    int newSize = size * 2;
+                    Entry[] newMap = new Entry[newSize];
+                    System.arraycopy(map, 0, newMap, 0, size);
+                    this.map = newMap;
+                    this.size = newSize;
+
+                    for (int i = this.cursor; i > position; i--) {
+                        this.map[i] = this.map[i - 1];
+                    }
+                    this.map[position] = new Entry(key, value);
+                    this.cursor++;
+                    result = true;
                 }
             } else {
-                int newSize = size * 2;
-                Entry[] newMap = new Entry[newSize];
-                System.arraycopy(map, 0, newMap, 0, size);
-                this.map = newMap;
-                this.size = newSize;
-
-                for (int i = 0; i < cursor; i++) {
-                    if (map[i].getUser().equals(key)) {
-                        tempValue = (E) map[i].getObject();
-                        map[i].setObject(value);
-                    } else {map[cursor++] = new Entry(key, value);}
-                }
+                return false;
             }
         }
-        return tempValue;
+        return result;
     }
 
     /**.
@@ -80,11 +95,25 @@ public class MyMap<U, E> {
      */
     public E get(U key) {
         if (key == null) {throw  new NullPointerException("Key is null");}
-        E result = null;
-        for (int i = 0; i < cursor; i++) {
-            if (map[i].getUser().equals(key)) {result = (E) map[i].getObject();}
-        }
-        return result;
+        int position = searchGetByKey(key, map, 0, cursor - 1);
+        if (position == -1) {throw new NoSuchElementException("The map does not contain this key");}
+        return (E) this.map[position].getObject();
+    }
+
+    /**.
+     * Method for delete element from the map
+     * @param key is key of the element
+     * @return boolean
+     */
+    public boolean remove(U key) {
+        int position = searchGetByKey(key, map, 0, cursor);
+        if (position != -1) {
+            for (int i = position; i < cursor; i++) {
+                this.map[i] = this.map[i + 1];
+            }
+            this.cursor--;
+            return true;
+        } else {return false;}
     }
 
     /**.
@@ -95,5 +124,117 @@ public class MyMap<U, E> {
         for (int i = 0; i < cursor; i++) {
             System.out.print(map[i].getObject() + " ,");
         }
+    }
+    public int searchInsert(U value, Entry[] array, int start, int end) {
+        int result = -1;
+        int low = start;
+        int high = end;
+        int mid = low + (high - low) / 2;
+        if (high == low && !array[0].getUser().equals(value)) {
+            if (value.hashCode() > array[low].getUser().hashCode()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if (high == low + 1) {
+            if (array[low].getUser().equals(value) || array[high].getUser().equals(value)) {return -1;}
+            if (value.hashCode() < array[low].getUser().hashCode()) {return 0;}
+            else if (value.hashCode() < array[high].getUser().hashCode()) {return 1;}
+            else {return 2;}
+        }
+
+        if (start > end || value == null || array == null) {return -1;}
+
+        if (value.hashCode() < array[mid].getUser().hashCode()) {
+            if (((mid - 1) - low) == 1 || ((mid - 1) == low)) {
+                if (!array[mid - 1].getUser().equals(value) && !array[low].getUser().equals(value)) {return mid - 1;}}
+            return searchInsert(value, array, low, mid - 1 );
+        } else if (value.hashCode() > array[mid].getUser().hashCode()) {
+            if ((high - (mid + 1)) == 1 || high == (mid + 1)) {
+                if (!array[mid + 1].getUser().equals(value) && !array[high].getUser().equals(value)) {return high;}}
+            return searchInsert(value, array, mid + 1, high);
+        } else if (array[mid].hashCode() ==value.hashCode()) {return -1;}
+        else {return result;}
+    }
+
+    public int searchGetByKey(U value, Entry[] array, int start, int end) {
+        if (start > end || value == null || array == null) {
+            return -1;
+        }
+        int result = -1;
+        int low = start;
+        int high = end;
+        int mid = low + (high - low) / 2;
+        if (high == low) {
+            if (value.hashCode() > array[low].getUser().hashCode()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if (high == low + 1) {
+            if (value.hashCode() == array[low].getUser().hashCode()) {
+                return low;
+            } else if (value.hashCode() == array[high].getUser().hashCode()) {
+                return high;
+            } else {
+                return -1;
+            }
+        }
+
+        if (value.hashCode() < array[mid].getUser().hashCode()) {
+            if (((mid - 1) - low) == 1 || ((mid - 1) == low)) {
+                if (array[mid - 1].getUser().hashCode() == value.hashCode()) {
+                    return mid - 1;
+                }
+                if (array[low].getUser().hashCode() == value.hashCode()) {
+                    return low;
+                }
+                return searchGetByKey(value, array, low, mid - 1);
+            }
+        } else if (value.hashCode() > array[mid].getUser().hashCode()) {
+            if ((high - (mid + 1)) == 1 || high == (mid + 1)) {
+                if (array[mid + 1].getUser().hashCode() == value.hashCode()) {
+                    return mid + 1;
+                }
+                if (array[high].getUser().hashCode() == value.hashCode()) {
+                    return high;
+                }
+                return searchGetByKey(value, array, mid + 1, high);
+            }
+        } else if (array[mid].hashCode() == value.hashCode()) {
+            return mid;
+        }
+    return result;
+    }
+    /**.
+     * Realisation iterator
+     * @return iterator
+     */
+    @Override
+    public Iterator<Entry> iterator() {
+        Iterator<Entry> it = new Iterator<Entry>() {
+
+            @Override
+            public boolean hasNext() {
+                return cursorIter < cursor;
+            }
+
+            @Override
+            public Entry next() {
+                if (hasNext()) {return map[cursorIter++];}
+                else {throw new NoSuchElementException("Need add cell in this array");}
+            }
+
+            @Override
+            public void remove() {
+                map[cursorIter] = null;
+                cursorIter--;
+            }
+        };
+        return it;
     }
  }
--- chapter_003/map/src/test/java/ru/job4j/MyMapTest.java
+++ chapter_003/map/src/test/java/ru/job4j/MyMapTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
- * Task 5.5.2.
+ * Task 5.5.2. and Task 5.5.8.
  * Create test for my map
  *
  * Created by ANTON VASILYUK on 21.06.2017.
@@ -19,6 +19,7 @@ public class MyMapTest {
      * @map is link to the map
      */
     MyMap map;
+    MyMap mapSecond;
 
     /**.
      * Is prepating for testing
@@ -26,20 +27,22 @@ public class MyMapTest {
     @Before
     public void preparingFoeTest() {
         map = new MyMap(3);
+        mapSecond = new MyMap(3);
     }
 
     /**.
      * Test working method put when add two time with one the key
      */
     @Test
-    public void whenPutTwoElementWithSemilarKey() {
+    public void whenInsertTwoElementWithSemilarKeyThenReturnFalse() {
         User userFirst = new User("Egor", 2, 1985, 5, 25);
         User userSecond = new User("Egor", 2, 1985, 5, 25);
         String testFirst = "First object";
         String testSecond = "Second object";
-        map.put(userFirst, testFirst);
-        map.put(userSecond, testSecond);
+        map.insert(userFirst, testFirst);
+        boolean result = map.insert(userSecond, testSecond);
         map.print();
+        assertThat(result, is(false));
     }
 
     /**.
@@ -49,7 +52,7 @@ public class MyMapTest {
     public void whenGetElementByKey() {
         User user = new User("Egor", 2, 1985, 5, 25);
         String testFirst = "Bingo";
-        map.put(user, testFirst);
+        map.insert(user, testFirst);
         String testSecond = (String) map.get(user);
         boolean result = false;
         if (testFirst.equals(testSecond)) {result = true;}
@@ -60,11 +63,11 @@ public class MyMapTest {
      * Test working put when key is null
      */
     @Test
-    public void whenPutKeyIsNull() {
+    public void whenInsertKeyIsNull() {
         User user = null;
         String test = "Test";
         try {
-            map.put(user, test);
+            map.insert(user, test);
         } catch (NullPointerException npe) {
             assertThat(npe.getMessage(), is("Key is null"));
         }
@@ -74,11 +77,11 @@ public class MyMapTest {
      * Test working put when value is null
      */
     @Test
-    public void whenPutValueIsNull() {
+    public void whenInsertValueIsNull() {
         User user = new User("Egor", 2, 1985, 5, 25);
         String test = null;
         try {
-            map.put(user, test);
+            map.insert(user, test);
         } catch (NullPointerException npe) {
             assertThat(npe.getMessage(), is("Element is null"));
         }
@@ -95,5 +98,58 @@ public class MyMapTest {
         } catch (NullPointerException npe) {
             assertThat(npe.getMessage(), is("Key is null"));
         }
+    }
+
+    /**.
+     * Test method Remove
+     */
+    @Test
+    public void whenRemoveElement() {
+        User userOne = new User("Egor", 2, 1985, 5, 25);
+        String one = "Test";
+        map.insert(userOne, one);
+        boolean result = map.remove(userOne);
+        assertThat(result, is(true));
+    }
+
+    /**.
+     * Test working iterator
+     */
+    @Test
+    public void whenMapContainTwhElementAndTwoTimeNextThenHasNextReturnFalse() {
+        User userFirst = new User("Ivan", 3, 1981, 5, 21);
+        User userSecond = new User("Egor", 2, 1985, 5, 25);
+        String testFirst = "First object";
+        String testSecond = "Second object";
+        map.insert(userFirst, testFirst);
+        map.insert(userSecond, testSecond);
+        map.iterator().next();
+        map.iterator().next();
+        boolean result = map.iterator().hasNext();
+        assertThat(result, is(false));
+    }
+
+    /**.
+     * Test
+     */
+    @Test
+    public void whenCheckSpeedWorkingInsert() {
+        long timeFirst = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            map.insert(new User(String.format("$s", i), 1, 2000, 1, 31), "Test");
+        }
+        long timeSecond = System.currentTimeMillis();
+        long resultOne = timeSecond - timeFirst;
+
+        timeFirst = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            mapSecond.insert(new User(String.format("$s", i), 1, 2000, 1, 31), "Test");
+        }
+        timeSecond = System.currentTimeMillis();
+        long resultTwo = timeSecond - timeFirst;
+
+        boolean result = false;
+        if ((resultOne - resultTwo) < 1000) {result = true;}
+        assertThat(result, is(true));
     }
 }
