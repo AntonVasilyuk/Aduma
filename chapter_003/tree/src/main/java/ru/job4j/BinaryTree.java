0a693361200f1019b59ad996df3b2a64baa71166
AntonVasilyuk <wrajina99@gmail.com>
2017-07-01 08:10:57 +0500

    add file for chapter_003_5.6.3

---
+++ chapter_003/tree/src/main/java/ru/job4j/BinaryTree.java
@@ -0,0 +1,151 @@
+package ru.job4j;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * Task 5.6.3.
+ * Create my Binary Tree
+ *
+ * Created by Anton Vasilyuk on 30.06.2017.
+ * @version 1.0
+ */
+public class BinaryTree<E extends Comparable<E>> implements Iterable<E>{
+
+    /**.
+     * @cursorIterator is position the iterator
+     */
+    private int cursorIterator = 0;
+
+    /**.
+     * @firstNode is first node in the tree
+     */
+    private Node<E> firstNode = null;
+
+    /**.
+     * @nodeForPaste is node  for paste element
+     */
+    //private Node<E> nodeForPaste;
+
+    /**.
+     * @list is container for elements
+     */
+    List<E> list;
+
+    /**.
+     * Constructor for class BinaryTree
+     */
+    public BinaryTree() {
+        list = new ArrayList<>();
+    }
+
+    /**.
+     * Class Node is container for elements and link to other elements
+     * @param <E>
+     */
+    private class Node<E> {
+        Node<E> childenLeft;
+        Node<E> childenRight;
+        E value;
+        Node<E> parentNode;
+        Node(E value, Node<E> node) {
+            this.value = value;
+            this.parentNode = node;
+        }
+    }
+
+    /**.
+     * Method for adding element to the tree
+     * @param value is element
+     */
+    public void add(E value) {
+        if (value == null) {throw new NullPointerException("Value is null");}
+        if (firstNode == null) {
+            firstNode = new Node(value, null);
+        } else {
+            searchAndPaste(firstNode, value);
+        }
+    }
+
+    /**.
+     * Method for search node for paste the element
+     * @param node is node
+     * @param value is element
+     */
+    private void searchAndPaste(Node<E> node, E value) {
+        if(node.value.compareTo(value) == 1) {
+            if(node.childenLeft == null) {node.childenLeft = new Node(value, node);}
+            else {searchAndPaste(node.childenLeft, value);}
+        } else {
+            if(node.childenRight == null) {node.childenRight = new Node(value, node);}
+            else {searchAndPaste(node.childenRight, value);}
+        }
+    }
+
+    /**.
+     * Method for fill and sorting the list
+     * @param node
+     */
+    public void fillListTwo(Node<E> node) {
+
+        if (firstNode.childenLeft == null && firstNode.childenRight == null) {list.add(node.value);}
+        if (list.size() != 0) {
+            if (list.contains(node.value)) {
+                if (node.parentNode != null) {
+                    fillListTwo(node.parentNode);
+                } else {
+                    return;
+                }
+            }
+        }
+
+        if(node.childenLeft != null && !list.contains(node.childenLeft.value)) {
+            if(node.childenLeft.childenLeft == null && node.childenLeft.childenRight == null) {
+                list.add(node.childenLeft.value);
+                list.add(node.value);
+                if(node.childenRight != null) {
+                    if (node.childenRight.childenLeft != null || node.childenRight.childenRight != null) {
+                        fillListTwo(node.childenRight);
+                    } else {
+                        list.add(node.childenRight.value);
+                        if (node.parentNode != null) {
+                            fillListTwo(node.parentNode);
+                        } else {return;}
+                    }
+                }
+            } else {fillListTwo(node.childenLeft);}
+        }
+
+        if (node.childenRight != null && !list.contains(node.childenRight.value)) {
+            if (node.childenRight.childenLeft == null && node.childenRight.childenRight == null) {
+                list.add(node.childenRight.value);
+                fillListTwo(node.parentNode);
+            } else {fillListTwo(node.childenRight);}
+        }
+    }
+
+    /**.
+     * Realisation iterator for binary tree
+     * @return iterator
+     */
+    @Override
+    public Iterator<E> iterator() {
+        Iterator<E> it = new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return list.size() > cursorIterator;
+            }
+
+            @Override
+            public E next() {
+                fillListTwo(firstNode);
+                if(hasNext()) {
+                    return list.get(cursorIterator++);
+                } else {throw new NoSuchElementException("No more elements");}
+            }
+        };
+        return it;
+    }
+}
---
+++ chapter_003/tree/src/test/java/ru/job4j/BinaryTreeTest.java
@@ -0,0 +1,94 @@
+package ru.job4j;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Task 5.6.3.
+ * Create test for my BinaryTree
+ *
+ * Created by Anton Vasilyuk on 01.07.2017.
+ * @version 1.0
+ */
+public class BinaryTreeTest {
+
+    /**.
+     * @link is link to the BinaryTree
+     */
+    BinaryTree link;
+
+    /**.
+     * Preparing for test Binary Tree
+     */
+    @Before
+    public void preparingForTest() {link = new BinaryTree();}
+
+    /**.
+     * Test method add
+     */
+    @Test
+    public void whenAddElementToTheTreeThenItarateReturnThisElement() {
+        String test = "Bingo!";
+        link.add(test);
+        String result = (String) link.iterator().next();
+        assertThat(result, is("Bingo!"));
+    }
+
+    /**.
+     * Test adding null element to the Tree
+     */
+    @Test
+    public void whenAddNullElement() {
+        String test = null;
+        try{
+            link.add(test);
+        } catch (NullPointerException npe) {
+            assertThat(npe.getMessage(), is("Value is null"));
+        }
+    }
+
+    /**.
+     * Test work sorting Tree
+     */
+    @Test
+    public void whenAddTreeElementThenReturnSortingElements() {
+        String one = "A";
+        String two = "B";
+        String three = "C";
+
+        link.add(two);
+        link.add(one);
+        link.add(three);
+
+        String[] array = new String[3];
+        String[] test = {one, two, three};
+
+        boolean result = true;
+        for(int i = 0; i < 3; i++) {
+            array[i] = (String) link.iterator().next();
+            if(!array[i].equals(test[i])) {result = false;}
+        }
+
+        assertThat(result, is(true));
+    }
+
+    /**.
+     * Test method hasNext
+     */
+    @Test
+    public void whenAddElementAndIterateOneTimeThenHasNextReturnNoMoreElement() {
+        String test = "Bingo";
+        link.add(test);
+        link.iterator().next();
+        try {
+            link.iterator().hasNext();
+        } catch (NoSuchElementException nee) {
+            assertThat(nee.getMessage(), is("No more elements"));
+        }
+    }
+}
