dc2e078e3bbce7869b6f4f5a19ccd3fa6c9c803c
AntonVasilyuk <wrajina99@gmail.com>
2017-07-20 05:49:15 +0500

    add file for chapter_004_7.2.1

---
+++ chapter_004/JMM/src/main/java/job4j/ru/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Implementation of multithreading issues.
+ *
+ * @author Anton Vasilyuk  (wrajina99@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package job4j.ru;
---
+++ chapter_004/JMM/src/test/java/job4j/ru/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Test class CheckProblem.
+ *
+ * @author Anton Vasilyuk  (wrajina99@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package job4j.ru;
