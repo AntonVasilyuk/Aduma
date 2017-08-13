09db8d7fec566892ab104aab27286b60fa88d095
Anton Vasilyuk <wrajina99@gmail.com>
2017-08-13 17:15:54 +0500

    add file for chapter_004_7.4.3

---
+++ chapter_004/Lock/src/main/java/ru/job4j/MyLock.java
@@ -0,0 +1,49 @@
+package ru.job4j;
+
+/**.
+ * Task 7.4.3.
+ * Create my realisation lock
+ *
+ * @autor Anton Vasilyuk on 13.08.2017
+ * @version 1.0.
+ */
+public class MyLock {
+
+    /**.
+     * @marker is marker for checking to locking
+     */
+    private boolean marker;
+
+    /**.
+     * Constructor for class MyLock
+     */
+    public MyLock() {
+        this.marker = true;
+    }
+
+    /**.
+     * Method for install status locking
+     */
+    public void lock() {
+        synchronized (this) {
+            while (marker) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            this.marker = true;
+        }
+    }
+
+    /**.
+     * Method for install status to unlocking
+     */
+    public void unlock() {
+        synchronized (this) {
+            notifyAll();
+            this.marker = false;
+        }
+    }
+}
---
+++ chapter_004/Lock/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Test realisation lock.
+ *
+ * @author Anton Vasilyuk  (wrajina99@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j;
