7a0e28fd6876035ea4aee2fde2dc56f06d3f03aa
Anton Vasilyuk <wrajina99@gmail.com>
2017-10-28 11:12:27 +0500

    add file for chapter_005_8.1.1

---
+++ chapter_005/Basic_function/src/main/resources/create.sql
@@ -0,0 +1,52 @@
+﻿create database java_a_to_z;
+
+create table right_roles (
+id_rr serial primary key,
+name_rr character varying (1000) not null unique
+);
+
+create table roles (
+id_r serial primary key,
+name_roles character varying (1000) not null unique,
+id_rr integer references right_roles (id_rr)
+);
+
+create table users (
+id_u serial primary key,
+name_user character varying (1000),
+login character varying (100),
+password character varying (100),
+right_role integer references right_roles (id_rr),
+role integer references roles (id_r) 
+);
+
+create table categorys (
+id_c serial primary key,
+name_category character varying (1000) not null unique
+);
+
+create table status_application (
+id_s serial primary key,
+name_status character varying (1000) not null unique
+);
+
+create table files (
+id_f serial primary key,
+link_to_file character varying (1000)
+);
+
+create table applications (
+id_a serial primary key,
+description character varying (1000),
+comment character varying (3000),
+file integer references files (id_f)
+);
+
+create table items (
+id_item serial primary key,
+description_application integer references applications (id_a),
+user_applicant integer references users (id_u),
+user_perfomers integer references users (id_u),
+category integer references categorys (id_c),
+status integer references status_application (id_s)
+);
---
+++ chapter_005/Basic_function/src/main/resources/insert.sql
@@ -0,0 +1,30 @@
+﻿insert into right_roles (name_rr) values ('admin');
+insert into right_roles (name_rr) values ('user');
+
+insert into roles (name_roles) values ('economist');
+insert into roles (name_roles) values ('network_administrator');
+
+insert into users (name_user, login, password, right_role, role) 
+values ('Anton', 'login', 'password', 5, 6);
+insert into users (name_user, login, password, right_role, role) 
+values ('Andrey', 'login', 'password', 6, 5);
+
+insert into categorys (name_category) values ('network_setup');
+insert into categorys (name_category) values ('payroll');
+
+insert into status_application (name_status) values ('runs');
+insert into status_application (name_status) values ('made');
+
+insert into files (link_to_file) values ('c:\dir\instruction.txt');
+
+insert into applications (description, comment, file) 
+values ('installation of a new computer', 'needs to be done very quickly!',
+1);
+
+insert into applications (description, comment) 
+values ('to calculate annual salary', 'asked the network administrator');
+
+insert into items (description_application, user_applicant, user_perfomers, category, status)
+values (1, 4, 5, 1, 1);
+insert into items (description_application, user_applicant, user_perfomers, category, status)
+values (2, 5, 4, 2, 1);
