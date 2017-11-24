e91d56e707802bffccf10f7a26233cb4a97d5ed3
Anton Vasilyuk <wrajina99@gmail.com>
2017-11-24 06:24:29 +0500

    add file for chapter_005_8.4.1

--- chapter_005/JDBC/src/main/java/ru/job4j/SQLStorage.java
+++ chapter_005/JDBC/src/main/java/ru/job4j/SQLStorage.java
@@ -2,10 +2,8 @@ package ru.job4j;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.rmi.runtime.Log;
-
 import java.sql.*;
-import java.util.Properties;
+
 
 public class SQLStorage {
     private final static Logger log = LoggerFactory.getLogger(SQLStorage.class);
