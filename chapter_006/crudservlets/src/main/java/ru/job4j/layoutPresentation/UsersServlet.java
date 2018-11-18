d8af41c8c80de50cb3fb970554845eacdc30ce2e
Aduma <wrajina99@gmail.com>
2018-11-18 11:30:09 +0500

    update file for chapter_006_9.8.1

--- chapter_006/crudservlets/src/main/java/ru/job4j/layoutPresentation/UsersServlet.java
+++ chapter_006/crudservlets/src/main/java/ru/job4j/layoutPresentation/UsersServlet.java
@@ -1,5 +1,6 @@
 package ru.job4j.layoutPresentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.job4j.layoutLogic.ValidateService;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class UsersServlet extends HttpServlet {
@@ -40,5 +42,14 @@ public class UsersServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute("users", logic.getListStorage());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("test/json");
+        PrintWriter writer = resp.getWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        writer.append(mapper.writeValueAsString(logic.getListStorage()));
+        writer.flush();
     }
 }
