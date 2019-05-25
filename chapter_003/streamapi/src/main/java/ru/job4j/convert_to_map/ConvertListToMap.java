c40bc4cecf757f8de3c8595a85064f87d5a46b60
vasanton <wrajina99@gmail.com>
2019-05-25 10:35:00 +0500

    update file for chapter_003.05.03

---
+++ chapter_003/streamapi/src/main/java/ru/job4j/convert_to_map/ConvertListToMap.java
@@ -0,0 +1,30 @@
+package ru.job4j.convert_to_map;
+
+import ru.job4j.school.Student;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**.
+ * Chapter_003
+ * Task_110226
+ * Converting list to map, key is family and value is object student
+ *
+ * @author Anton Vasilyuk
+ * @version 1.0
+ * @since 0.1
+ */
+public class ConvertListToMap {
+
+    /**.
+     * Method for converting list students to map, where key is family and value is student
+     * @param listStudent is list student
+     * @return converted map
+     */
+    public Map<String, Student> convert(List<Student> listStudent) {
+        return listStudent.stream()
+                .distinct()
+                .collect(Collectors.toMap(k -> k.getFamily(), v -> v));
+    }
+}
---
+++ chapter_003/streamapi/src/main/java/ru/job4j/convert_to_map/package-info.java
@@ -0,0 +1,9 @@
+/**.
+ * Chapter_003.
+ * App for converting list student to map students where key is family and value is object student
+ *
+ * @author Anton Vasilyuk (wrajina99@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.convert_to_map;
