7006d14de5d919bcc7f160e92e731b74273e76e4
vasanton <wrajina99@gmail.com>
2019-05-26 09:47:27 +0500

    update file for chapter_003.05.04

--- chapter_003/streamapi/src/main/java/ru/job4j/address/Profiles.java
+++ chapter_003/streamapi/src/main/java/ru/job4j/address/Profiles.java
@@ -23,13 +23,13 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .distinct()
                 .sorted(new Comparator<Address>() {
                     @Override
                     public int compare(Address o1, Address o2) {
                         return o1.getCity().compareTo(o2.getCity());
                     }
                 })
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
